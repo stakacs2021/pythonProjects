365b1f8b9f902c7962126edbab3831fb9289ff3c
stakacs2021 <takacsstephan@gmail.com>
2024-12-21 23:31:03 -0800

    changed lots of stuff
    changed to docker compose file for nginx revproxy

--- fileServer/nodeapp/server.js
+++ fileServer/nodeapp/server.js
@@ -84,7 +84,7 @@ app.post("/upload", (req, res) => {
 });
 
 // Start the server
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
